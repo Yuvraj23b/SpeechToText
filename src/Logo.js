f34832cd9d0ef9ab861f6f49bfd3c58cd0eae70c
Yuvraj23b <yuvrajbrahmbhatt21@gnu.ac.in>
2023-09-10 14:23:44 +0530

    first commit

---
+++ src/Logo.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './Logo.css'; // Import your logo's CSS file
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      Yuvraj Brahmbhatt
+    </div>
+  );
+};
+
+export default Logo;
